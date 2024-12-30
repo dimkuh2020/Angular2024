98ad8cea495a5958ef36a32b9809388ea74ab31b
dima <dimkuh2020@gmail.com>
2024-12-31 01:00:51 +0200

    api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProfileCardComponent } from "./common-ui/profile-card/profile-card.component";
+import { ProfileService } from './data/services/profile.service';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProfileCardComponent],
+  imports: [ProfileCardComponent, JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'tik-talk2024';
+  profileService = inject(ProfileService);
+  profiles: any = [];
+
+  constructor() {
+    this.profileService.getTestAccounts().subscribe((val) => {
+      this.profiles = val;
+    })
+  }
 }
